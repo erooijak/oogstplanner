aa6ab55b1efaaca5e729cb857d9ff7252c646c0e
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2014-11-21 18:15:28 +0100

    Initial update to data model: foreign keys and JSON storage of farming months

--- Zk/Models/Calendar.cs
+++ Zk/Models/Calendar.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Zk.Models
 {
+	/// <summary>
+	/// 	A calendar contains the farming months with sowing and harvesting patterns for a user.
+	/// </summary>
 	public class Calendar
 	{
-		public User User { get; set; }
-		public IList<FarmingMonth> FarmingMonths { get; set; }
+		[ForeignKey("User")]
+		public int UserId { get; set; } // One-to-one relationship (each calendar belongs to one user)
+
+		public virtual User User { get; set; }
+		public virtual ICollection<FarmingMonth> FarmingMonths { get; set; } // One-to-many (each calendar has many
+																			 // farming months)
+
 	}
 }
