6932ad8c2660446b1ba73aa60b09527cc5e8ffd9
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2014-11-22 15:55:43 +0100

    Moved stylesheets, added images per season plus script to convert quality and filenames

---
+++ Zk/Content/Images/convert-images.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Summary: 
+#   This script will change all .jpg images collected in the Winter, Summer, Autumn and Spring folders 
+#   to 80% quality and a width of 300 pixels while keeping original ratio. Files are renamed with 
+#   increasing numbers per folder.
+#   Note: original images will be removed.
+#
+# Dependency: 
+#   ImageMagick.
+#
+# Usage:
+#   Place script in root of images folder and run with "bash convert-images.sh".
+
+seasons=("Spring" "Summer" "Autumn" "Winter");
+
+for season in "${seasons[@]}"; do
+  i=1;
+  for image in $season/*.jpg; do
+    convert $image -resize 300x -quality 80% $season/$i.jpg;
+    rm $image;
+    ((i++));
+  done
+done
+
+echo "All jpgs are converted to 300 pixels and 80% quality."
