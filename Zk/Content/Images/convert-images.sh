03b5fac66bfaaaa97b4c78739f2cff0b6c02ed38
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2014-11-22 18:15:58 +0100

    Redo images, add images to home page and make text responsive with flowtype

--- Zk/Content/Images/convert-images.sh
+++ Zk/Content/Images/convert-images.sh
@@ -2,8 +2,8 @@
 
 # Summary: 
 #   This script will change all .jpg and .jpeg images collected in the Winter, Summer, Autumn and Spring 
-#   folders to 80% quality and a width of 300 pixels while keeping original ratio. Files are renamed with 
-#   increasing numbers per folder. Images were obtained from http://pixabay.com/.
+#   folders to 95% quality and a width of 500 pixels while keeping original ratio. Files are renamed with 
+#   increasing numbers per folder. Images were obtained from http://pixabay.com/ (small format).
 #   Note: original images will be removed.
 #
 # Dependency: 
@@ -17,10 +17,10 @@ seasons=("Spring" "Summer" "Autumn" "Winter");
 for season in "${seasons[@]}"; do
   i=1;
   for image in $season/*.jp*; do
-    convert $image -resize 300x -quality 80% $season/$i.jpg;
+    convert $image -resize 500x -quality 95% $season/$i.jpg;
     rm $image;
     ((i++));
   done
 done
 
-echo "All jp(e)gs are converted to 300 pixels and 80% quality."
+echo "All jp(e)gs are converted to 500 pixels and 95% quality."
