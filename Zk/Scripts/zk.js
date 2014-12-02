0aacc32b5fbb147d8fa7c4f327fa5668be966a33
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2014-12-03 00:15:25 +0100

    Display some data on the farming months view with zk.js plus some styling/images for mentioned view

--- Zk/Scripts/zk.js
+++ Zk/Scripts/zk.js
@@ -6,6 +6,7 @@
     
     toMain: function() {
         $.fn.fullpage.moveSlideLeft();
+        this.emptyFarmingMonth();
     },
 
     fillFarmingMonth: function(data) {
@@ -13,7 +14,52 @@
         // Display month on top of page
         $('#current-month').text(data.month);
 
-        // TODO: Display pattern for edit
+        // Display all crops that need to be sowed and harvested.
+        var sowingActions = this.getData(data.farmingActions, "Action", "Sowing");
+        var harvestingActions = this.getData(data.farmingActions, "Action", "Harvesting");
+
+        this.fillSowingPattern(sowingActions);
+        this.fillHarvestingPattern(harvestingActions);
+       
+    },
+
+    fillSowingPattern: function(actions) {
+        // Display name, Race, and Crop count in the sowing table.
+        actions.forEach(function (a) {
+            $('#sowing').append( $(["<tr>",
+                                "  <td>" + a.Crop.Name      + "</td>",
+                                "  <td>" + a.Crop.Race      + "</td>",
+                                "  <td>" + a.Crop.Category  + "</td>",
+                                "  <td>" + a.CropCount      + "</td>",
+                                "</tr>"
+                               ].join("\n")));  
+        });
+    },
+
+    fillHarvestingPattern: function(actions) {
+        // Display name, Race, and Crop count in the sowing table.
+        actions.forEach(function (a) {
+            $('#harvesting').append( $(["<tr>",
+                                "  <td>" + a.Crop.Name      + "</td>",
+                                "  <td>" + a.Crop.Race      + "</td>",
+                                "  <td>" + a.Crop.Category  + "</td>",
+                                "  <td>" + a.CropCount      + "</td>",
+                                "</tr>"
+                               ].join("\n")));  
+        });
+    },
+
+    emptyFarmingMonth: function() {
+        $('#sowing').empty();
+        $('#harvesting').empty();
+    },
+
+    // Helper functions:
+    //   Returns the elements from an array (arr) where the type (type) has the specified value (val).
+    getData: function(arr, type, val) {
+        return arr.filter(function (el) {
+            return el[type] === val;
+        });
     }
 
 };
