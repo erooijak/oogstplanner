8262b77a3678d030e201f01cc48422ffe3e08e22
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2014-12-25 15:02:38 +0100

    Fix indentation global.asax

--- Zk/Global.asax.cs
+++ Zk/Global.asax.cs
@@ -11,37 +11,62 @@ using Zk.Models;
 
 namespace Zk
 {
-	public class MvcApplication : HttpApplication
-	{
+    public class MvcApplication : HttpApplication
+    {
 
-		public static void RegisterRoutes(RouteCollection routes)
-		{
-			routes.IgnoreRoute ("{resource}.axd/{*pathInfo}");
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute ("{resource}.axd/{*pathInfo}");
 
-			routes.MapRoute (
-				"Default",
-				"{controller}/{action}/{id}",
-				new { controller = "Home", action = "Index", id = "" }
-			);
+            routes.MapRoute (
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = "" }
+            );
 
-		}
+        }
             
-		protected void Application_Start()
-		{
-            if (!WebSecurity.Initialized)
+        /* Return to cleaner URL, cannot be used with external logins */
+        void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+            var cookieName = FormsAuthentication.FormsCookieName;
+            var authCookie = Context.Request.Cookies[cookieName];
+            if (authCookie == null)
+            {
+                /*
+                 There is no authentication cookie. User is not authenticated.
+
+                 Instead of using asp.net built in redirect with the returnUrl querystring,
+                 this will redirect user to login.aspx.
+                 The url of the current page will be checked to prevent the user from
+                 being redirected to index.cshtml again when redirected to index.cshtml in
+                 the first place.
+                */
+                var url = Request.RawUrl.Split('?')[0];
+                if (url != "/Account/Login")
+                {
+                    Response.Redirect("/Account/Login");
+                    return;
+                }   
+            }
+        } 
+
+        protected void Application_Start()
+        {
+            if (!WebSecurity.Initialized) 
             {
                 WebSecurity.InitializeDatabaseConnection("ZkTestDatabaseConnection", "Users", "UserId", "Name", autoCreateTables: true);
             }
 
-			AreaRegistration.RegisterAllAreas();
+            AreaRegistration.RegisterAllAreas();
 
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
 
-		}
+        }
 
-	}
+    }
         
 }
