868c716c730d3d0f1dfe8ef73dfe55479df8aa2f
Erwin Rooijakkers <erwinrooijakkers@gmail.com>
2015-02-15 21:04:44 +0100

    Display action type in drag and drop explanation sentence; Remove new calendar view and controller action

--- Zk/Scripts/zk.crop-selection-box.js
+++ Zk/Scripts/zk.crop-selection-box.js
@@ -3,6 +3,7 @@
     /* Initially hide the selected crop info box */
     $('#selected-crop-info').hide();
 
+    /* Set information in the selected-crop box*/
     $('#crop-selection-box').on('typeahead:selected', function(e, datum) { 
         $('#selected-crop-info').show();
         /* TODO: Call server to get an image via datum.id (cropId) */
@@ -13,5 +14,11 @@
         $('#selected-crop-areaPerCrop').text(datum.areaPerCrop);
         $('#selected-crop-pricePerBag').text(datum.pricePerBag);
     })
+        
+    /* Display action type in the drag and drop explanation sentence.*/
+    $('input[name=action-type-radios]').on('change', function() {
+        var actionType = this.value;
+        $('#drag-and-drop-sentence-action-type').text(actionType.toLowerCase());
+    });
 
 });
