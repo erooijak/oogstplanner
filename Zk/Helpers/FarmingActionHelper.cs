8d9f091c1d27b6b7841f8804def5e153566b289c
erooijak <erwinrooijakkers@gmail.com>
2015-03-25 23:04:32 +0100

    Fix return type of FarmingActionHelper methods

--- Zk/Helpers/FarmingActionHelper.cs
+++ Zk/Helpers/FarmingActionHelper.cs
@@ -5,12 +5,12 @@ namespace Zk.Helpers
     public static class FarmingActionHelper
     {
 
-        public static void GetRelatedActionType(FarmingAction action, int cropGrowingTime)
+        public static ActionType GetRelatedActionType(FarmingAction action, int cropGrowingTime)
         {
             return action.Action == ActionType.Harvesting ? ActionType.Sowing : ActionType.Harvesting;
         }
             
-        public static void GetRelatedMonth(FarmingAction action, int cropGrowingTime)
+        public static Month GetRelatedMonth(FarmingAction action, int cropGrowingTime)
         {
             return action.Action == ActionType.Harvesting 
                 ? action.Month.Subtract(cropGrowingTime)
