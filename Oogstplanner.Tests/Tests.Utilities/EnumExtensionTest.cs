7cbf458bbcab5193191dbb43f98f0f0a4b6724e1
erooijak <erwinrooijakkers@gmail.com>
2015-05-10 18:19:34 +0200

    Add unit tests for enum extension

---
+++ Oogstplanner.Tests/Tests.Utilities/EnumExtensionTest.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+
+using Oogstplanner.Utilities.ExtensionMethods;
+
+namespace Oogstplanner.Tests.Utilities
+{
+    [TestFixture]
+    public class EnumExtensionTest
+    {
+        const string Description1 = "First description";
+        const string Description2 = "Second description";
+
+        // ARRANGE
+        enum Test1
+        { 
+            [System.ComponentModel.Description(Description1)]
+            One = 1, 
+            [System.ComponentModel.Description(Description2)]
+            Two = 2
+        }
+
+        [Flags]
+        enum Test2
+        { 
+            [System.ComponentModel.Description(Description1)]
+            One = 1, 
+            [System.ComponentModel.Description(Description2)]
+            Two = 2
+        }
+
+        [Test]
+        public void Utilities_EnumExtension_Description()
+        {
+            // ACT
+            var result = Test1.One.GetDescription();
+
+            // ASSERT
+            Assert.AreEqual(Description1, result, 
+                "Description should be obtained from normal enumeration.");
+        }
+
+        [Test]
+        public void Utilities_EnumExtension_DescriptionFlags()
+        {
+            // ACT
+            var result = Test2.Two.GetDescription();
+
+            // ASSERT
+            Assert.AreEqual(Description2, result, 
+                "Description should be obtained from flags enumeration.");
+        }
+
+    }
+}
