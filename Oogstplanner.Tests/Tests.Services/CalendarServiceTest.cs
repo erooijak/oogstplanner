776cd4b77d2fe6e8808cbfa37768624fbf29c7e5
erooijak <erwinrooijakkers@gmail.com>
2015-05-10 15:14:47 +0200

    Add unit tests calendar service

--- Oogstplanner.Tests/Tests.Services/CalendarServiceTest.cs
+++ Oogstplanner.Tests/Tests.Services/CalendarServiceTest.cs
@@ -2,10 +2,12 @@
 using NUnit.Framework;
 
 using System;
+using System.Collections.Generic;
 
+using Oogstplanner.Models;
+using Oogstplanner.ViewModels;
 using Oogstplanner.Repositories;
 using Oogstplanner.Services;
-using Oogstplanner.Models;
 using Oogstplanner.Tests.Lib.Fakes;
 
 namespace Oogstplanner.Tests.Services
@@ -45,7 +47,141 @@ namespace Oogstplanner.Tests.Services
             calendarRepositoryMock.Verify(mock =>
                 mock.GetCalendar(expectedUserId), Times.Once,
                 "The calendar for the user id should be retrieved");
-           
         }
+
+        [Test]
+        public void Services_Calendar_GetYearCalendar()
+        {
+            // ARRANGE
+            var calendarRepositoryMock = new Mock<ICalendarRepository>();
+            var farmingActionServiceMock = new Mock<IFarmingActionService>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var fakeUserServices = new FakeUserServices(
+                userRepositoryMock.Object, 
+                calendarRepositoryMock.Object);
+            var userServiceMock = new Mock<IUserService>();
+            var authenticationServiceMock = new Mock<IAuthenticationService>();
+
+            var expectedUserId = new Random().Next();
+
+            userServiceMock.Setup(mock => mock.GetCurrentUserId())
+                .Returns(expectedUserId);
+
+            fakeUserServices.ReturnedUserService = userServiceMock.Object;
+
+            var service = new CalendarService(
+                calendarRepositoryMock.Object, 
+                farmingActionServiceMock.Object,
+                fakeUserServices,
+                authenticationServiceMock.Object);
+
+            // ACT
+            var result = service.GetYearCalendar();
+
+            // ASSERT
+            farmingActionServiceMock.Verify(mock =>
+                mock.GetHarvestingActions(expectedUserId, It.IsAny<Month>()),
+                Times.Exactly(12),
+                "12 month calendars with harvesting actions should be retrieved " +
+                "for a year calendar.");
+            farmingActionServiceMock.Verify(mock =>
+                mock.GetSowingActions(expectedUserId, It.IsAny<Month>()),
+                Times.Exactly(12),
+                "12 month calendars with sowing actions should be retrieved " +
+                "for a year calendar.");
+            Assert.IsInstanceOf(typeof(YearCalendarViewModel), result,
+                "A YearCalendarViewModel should be returned");
+        }
+
+        [Test]
+        public void Services_Calendar_GetMonthCalendar()
+        {
+            // ARRANGE
+            var calendarRepositoryMock = new Mock<ICalendarRepository>();
+            var farmingActionServiceMock = new Mock<IFarmingActionService>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var fakeUserServices = new FakeUserServices(
+                userRepositoryMock.Object, 
+                calendarRepositoryMock.Object);
+            var userServiceMock = new Mock<IUserService>();
+            var authenticationServiceMock = new Mock<IAuthenticationService>();
+
+            var expectedUserId = new Random().Next();
+            var expectedMonth = Month.May;
+            var expectedHarvestingActions = new List<FarmingAction>();
+            var expectedSowingActions = new List<FarmingAction>();
+
+            userServiceMock.Setup(mock => mock.GetCurrentUserId())
+                .Returns(expectedUserId);
+
+            fakeUserServices.ReturnedUserService = userServiceMock.Object;
+
+            farmingActionServiceMock.Setup(mock =>
+                mock.GetHarvestingActions(
+                    expectedUserId,
+                    It.IsAny<Month>()))
+                .Returns(expectedHarvestingActions);
+
+            farmingActionServiceMock.Setup(mock =>
+                mock.GetSowingActions(
+                    expectedUserId,
+                    It.IsAny<Month>()))
+                .Returns(expectedSowingActions);
+
+            var service = new CalendarService(
+                calendarRepositoryMock.Object, 
+                farmingActionServiceMock.Object,
+                fakeUserServices,
+                authenticationServiceMock.Object);
+
+            // ACT
+            var result = service.GetMonthCalendar(expectedMonth);
+
+            // ASSERT
+            Assert.IsInstanceOf(typeof(MonthCalendarViewModel), result, 
+                "A MonthCalendarViewModel should be returned.");
+            Assert.AreEqual(expectedHarvestingActions, result.HarvestingActions,
+                "The harvesting actions retrieved from the service should be in the view model.");
+            Assert.AreEqual(expectedSowingActions, result.SowingActions,
+                "The harvesting actions retrieved from the service should be in the view model.");
+            Assert.IsNotEmpty(result.DisplayMonth,
+                "Display month should have a value.");
+        }
+
+        [Test]
+        public void Services_Calendar_GetMonthWithAction()
+        {
+            // ARRANGE
+            var calendarRepositoryMock = new Mock<ICalendarRepository>();
+            var farmingActionServiceMock = new Mock<IFarmingActionService>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var fakeUserServices = new FakeUserServices(
+                userRepositoryMock.Object, 
+                calendarRepositoryMock.Object);
+            var userServiceMock = new Mock<IUserService>();
+            var authenticationServiceMock = new Mock<IAuthenticationService>();
+
+            var expectedUserId = new Random().Next();
+
+            userServiceMock.Setup(mock => mock.GetCurrentUserId())
+                .Returns(expectedUserId);
+
+            fakeUserServices.ReturnedUserService = userServiceMock.Object;
+
+            var service = new CalendarService(
+                calendarRepositoryMock.Object, 
+                farmingActionServiceMock.Object,
+                fakeUserServices,
+                authenticationServiceMock.Object);
+
+            // ACT
+            service.GetMonthsWithAction();
+
+            // ASSERT
+            calendarRepositoryMock.Verify(mock =>
+                mock.GetMonthsWithAction(expectedUserId), Times.Once,
+                "The get months with action method should be called with the user id.");
+        }
+
     }
 }
