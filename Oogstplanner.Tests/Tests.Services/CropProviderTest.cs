3b4663203db82dd1d72377238b6ad7391833abe9
erooijak <erwinrooijakkers@gmail.com>
2015-05-10 15:56:13 +0200

    Add unit tests for crop provider

---
+++ Oogstplanner.Tests/Tests.Services/CropProviderTest.cs
@@ -0,0 +1,55 @@
+﻿using Moq;
+using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+
+using Oogstplanner.Models;
+using Oogstplanner.ViewModels;
+using Oogstplanner.Repositories;
+using Oogstplanner.Services;
+using Oogstplanner.Tests.Lib.Fakes;
+
+namespace Oogstplanner.Tests.Services
+{
+    [TestFixture]
+    public class CropProviderTest
+    {
+        [Test]
+        public void Services_Crop_GetAllCrops()
+        {
+            // ARRANGE
+            var cropRepositoryMock = new Mock<ICropRepository>();
+
+            var service = new CropProvider(cropRepositoryMock.Object);
+
+            // ACT
+            service.GetAllCrops();
+
+            // ASSERT
+            cropRepositoryMock.Verify(mock =>
+                mock.GetAllCrops(), Times.Once,
+                "All crops should be retrieved from the repository.");
+        }
+
+        [Test]
+        public void Services_Crop_GetById()
+        {
+            // ARRANGE
+            var cropRepositoryMock = new Mock<ICropRepository>();
+
+            var service = new CropProvider(cropRepositoryMock.Object);
+
+            var expectedCropId = new Random().Next();
+
+            // ACT
+            service.GetCrop(expectedCropId);
+
+            // ASSERT
+            cropRepositoryMock.Verify(mock =>
+                mock.GetCrop(expectedCropId), Times.Once,
+                "The crop with the expected id should be obtained from " +
+                "the repository.");
+        }
+    }
+}
