d3042fe45a16153f554ea21aa829bcb20c90d42b
erooijak <erwinrooijakkers@gmail.com>
2015-06-08 23:36:02 +0200

    Display likes and creation date on info page

--- Oogstplanner.Models/User.cs
+++ Oogstplanner.Models/User.cs
@@ -11,7 +11,8 @@ namespace Oogstplanner.Models
         }
 
         public int Id { get; set; }
-        public string Name { get; set; }	
+        public string Name { get; set; }
+
         public string FullName { get; set; }
         public string Email { get; set; }
         public AuthenticatedStatus AuthenticationStatus { get; set; }
