32fa4053f93b9c7de18a81502fd9b1daa9cea4c8
erooijak <erwinrooijakkers@gmail.com>
2015-06-21 23:10:48 +0200

    Order methods in membership service interface by type

--- Oogstplanner.Services/Contracts/IMembershipService.cs
+++ Oogstplanner.Services/Contracts/IMembershipService.cs
@@ -7,9 +7,9 @@ namespace Oogstplanner.Services
     public interface IMembershipService
     {
         bool ValidateUser(string userNameOrEmail, string password);
-        void SetAuthCookie(string userNameOrEmail, bool createPersistentCookie);
         MembershipUser GetMembershipUserByEmail(string email);
         bool TryCreateUser(string username, string password, string email, out ModelError modelError);
+        void SetAuthCookie(string userNameOrEmail, bool createPersistentCookie);
         void AddUserToRole(string userName, string role);
         void SignOut();
         void RemoveUser(string userName);
